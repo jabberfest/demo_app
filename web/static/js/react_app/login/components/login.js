56001ac7bf58db01ecd42478e8e7860a1ec117c0
Fernando <fernando@jabberfest.com>
2018-01-10 13:46:12 -0500

    Add SASS webpack support

--- web/static/js/react_app/login/components/login.js
+++ web/static/js/react_app/login/components/login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
+import style from 'css/react_app/login/login.scss';
+
 
 class Login extends React.Component{
     constructor(){
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,53 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./web/static/js/app.js",
-    output: {
-      path: path.join(__dirname, "/priv/static/js"),
-      filename: "app.js"
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-              test: /\.js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['env']
+  entry: "./web/static/js/app.js",
+  output: {
+    path: path.join(__dirname, "/priv/static/js"),
+    filename: "app.js"
+  },
+  devtool: 'cheap-eval-source-map',
+  module: {
+      rules: [
+              {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['env']
+                  }
                 }
+              },
+              {
+                test: /\.scss$/,
+                use: [
+                  {
+                    loader: 'style-loader' // inject CSS to page
+                  }, 
+                  {
+                    loader: 'css-loader' // translates CSS into CommonJS modules
+                  },
+                  {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                      plugins: function () { // post css plugins, can be exported to postcss.config.js
+                        return [
+                          require('precss'),
+                          require('autoprefixer')
+                        ];
+                      }
+                    }
+                  },{
+                    loader: 'sass-loader' // compiles Sass to CSS
+                  }
+                ]
               }
-            }
-          ]
+      ]
+  },
+  resolve: {
+    alias: {
+      css: path.resolve(__dirname, './web/static/css')
     }
+  }
 };
