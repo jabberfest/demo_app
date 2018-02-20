8133cafa8b1a23dc508143f6f9bb5d2eb37f6ee8
Fernando <fernando@jabberfest.com>
2018-02-20 03:12:12 -0500

    Add static private policy to satisy facebook app requirements

--- lib/demo/endpoint.ex
+++ lib/demo/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Demo.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :demo, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt privacy.html)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
