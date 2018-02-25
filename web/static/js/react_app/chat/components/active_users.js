b2106abc2d5bde63d64ea2dbf71f431de5b3b6bc
Fernando <fernando@jabberfest.com>
2018-02-25 05:50:14 -0500

    Add scroller to active users, make mobile friendly

--- web/static/js/react_app/chat/components/active_users.js
+++ web/static/js/react_app/chat/components/active_users.js
@@ -2,20 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
+// Views
+import CustomScroll from 'react-custom-scroll';
 
 const ActiveUsers = ({users}) => {
     return (        
         <div>
             <div className="active-user-count"><span>{users.length} Active Users</span></div>
-            {
-                users.map((user)=> 
-                    <div key={user.id} className="user">
-                        <img height="28" width="28" 
-                        src={""+user.avatar}/> 
-                        <span>{user.name.split(" ")[0]}</span>
-                    </div>
-                )
-            }        
+            <CustomScroll flex="1">
+                <div className="users">
+                    {
+                        users.map((user)=> 
+                            <div key={user.id} className="user">
+                                <img height="28" width="28" 
+                                src={""+user.avatar}/> 
+                                <span>{user.name.split(" ")[0]}</span>
+                            </div>
+                        )
+                    }    
+                </div>
+            </CustomScroll>
         </div>
     )
 }
