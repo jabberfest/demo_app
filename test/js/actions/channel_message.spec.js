84e87fd2bbbe3658f8a3bf29d5b0999352478118
Fernando <fernando@jabberfest.com>
2018-03-08 08:18:53 -0500

    Initial Jest setup

---
+++ test/js/actions/channel_message.spec.js
@@ -0,0 +1,8 @@
+import * as actions from 'react_app/chat/actions/channel_message';
+
+
+describe('actions', () => {
+    it('should pass the sample test', () => {
+      expect(1).toEqual(1)
+    })
+})
